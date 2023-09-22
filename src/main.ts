4815e24c95dd8ad9473520c95b445a4be18b093e
anom <dgg.photo.drive@gmail.com>
2023-09-22 10:32:27 +0700

    feat: restriction validate for extra field in client req payload and response

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // This will trim extra field in dto and only allow field in dto
+      whitelist: true,
+      // This will forbid extra field in dto in client payload
+      forbidNonWhitelisted: true
+    })
+  )
   await app.listen(6000)
 }
 bootstrap()
