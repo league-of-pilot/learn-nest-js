f14b20e2033962aea8f4f92c7bf3f966bc997214
anom <dgg.photo.drive@gmail.com>
2023-09-26 15:19:37 +0700

    feat: apply typeoOrm User to user module and findOne api

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from 'src/entities/user.entity'
+import { Repository } from 'typeorm'
 import { CreateUserDto } from './dto/createUserDto'
 
 @Injectable()
 export class UserService {
-  findOne(id: string) {
+  constructor(
+    @InjectRepository(User) private readonly userRepo: Repository<User>
+  ) {}
+
+  findOneDummy(id: string) {
     return {
       id: id,
       message: 'localhost:{PORT}/user/find/:id'
@@ -15,5 +22,9 @@ export class UserService {
   create(userDto: CreateUserDto) {
     console.count('🚀🚀 inside create by service')
     return userDto
+  }
+
+  async findOne(id: string) {
+    return await this.userRepo.findOne({ where: { id: id } })
   }
 }
